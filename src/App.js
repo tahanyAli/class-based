92e8755066ff037180d14cc591ae69109302e693
tahany777 <tahanymohammed777@hotmail.com>
2023-03-10 22:41:55 +0300

    feat: search box component and card list

--- src/App.js
+++ src/App.js
@@ -1,39 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
-import { Component } from 'react';
-// import React from 'react';
-
+import "./App.css";
+import { Component } from "react";
+import CardList from "./components/card-list/card-list.component";
+import SearchBox from "./components/search-box/Search-box.component";
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
-      name: 'Ahmad'
-    }
+      monsters: [],
+      searchField: "",
+    };
   }
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      );
+  }
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();
+
+    this.setState(() => {
+      // return { searchField: searchField };
+      //OR
+      return { searchField };
+    });
+  };
+
   render() {
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+
+    // const filteredMonsters = this.state.monsters.filter((monster) => {
+    //   return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+    // });
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-           {this.state.name}
-          </p>
-          <button onClick={() => {
-            this.setState({name: 'Andrei'})
-          }}>Update</button>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <SearchBox
+          onChangeHandler={onSearchChange}
+          placeholder="search monsters"
+          className="search-box"
+        />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
 }
-
 
 export default App;
